5cbd54573bed4a25ae87205dc307490bfab48852
TheOnlySG <theonlysg65@gmail.com>
2026-07-06 09:36:03 +0530

    solved LC 394

--- LC 503/code.py
+++ LC 503/code.py
@@ -1,18 +1,25 @@
 class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        n = len(s)
+        numbers = '1234567890'
+        for ch in s :
+            if ch != ']':
+                stack.append(ch)
+            else :
+                num = ''
+                pieces = []
+                while stack[-1] != '[' and stack[-1] not in numbers:
+                    pieces.append(stack.pop())
+
+                if stack[-1] == '[' : stack.pop()
+                pieces.reverse()
+                string = ''.join(pieces)
+                if stack :
+                    while stack and  stack[-1] in numbers:
+                        num = num + stack.pop()
+                num = 1 if num == '' else int(num[::-1]) 
+                stack.append(string*num)
         
-        #resultant is the result array , we are initializing it to -1 each element
-        resultant = [-1] * len(nums)
-        stack = [] #stack is going to hold indexes of the array elements
-
-        for i in range(2*len(nums)) :
-            current = nums[i%len(nums)] #since the loops is going to run 2 times due to 2*len(nums),
-            #we need to take remeinder in order to iterate 2 times
-            while stack and nums[stack[-1]] < current: #if current element is greater than the stacks top index
-                index = stack.pop() # take the index from stack , current is the next greater element of stacks top indexed element
-                resultant[index] = current #set the stacks top indexed element to current in resultant
-            
-            if i < len(nums) :
-                stack.append(i) # push the elements to stack
-
-        return resultant
+        return ''.join(stack)
+        
